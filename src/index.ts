c201eb21a18d9e2696ecdb648d8c25b9e481179d
genji-hackle <suhokim@hackle.io>
2025-04-18 14:09:36 +0900

    0.0.4

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 #!/usr/bin/env node
 
 import { MCPServer } from "mcp-framework";
+import packageJson from '../package.json' with { type: 'json' };
 
-const server = new MCPServer();
+const server = new MCPServer({
+  name: packageJson.name,
+  version: packageJson.version,
+  basePath: "./dist",
+});
 
 server.start();
+
+process.on('SIGINT', async () => {
+    await server.stop();
+});
