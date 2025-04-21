e544c647b212e02a98adbc1f0cc6e6054331dc3c
genji-hackle <suhokim@hackle.io>
2025-04-21 11:43:01 +0900

    0.0.6

--- src/tools/ExperimentListTool.ts
+++ src/tools/ExperimentListTool.ts
@@ -5,6 +5,8 @@ import WebClient from "../module/WebClient.js";
 interface ExperimentListInput {
   pageNumber?: number;
   pageSize?: number;
+  experimentStatusList?: string[];
+  searchKeyword?: string;
 }
 
 class ExperimentListTool extends MCPTool<ExperimentListInput> {
@@ -22,11 +24,28 @@ class ExperimentListTool extends MCPTool<ExperimentListInput> {
       description: "Number of items to display per page (min 1, default 100)",
       default: 100,
     },
+    searchKeyword: {
+      type: z.string().optional(),
+      description:
+        "Parameter used to filter experiments by keyword. Allows searching for experiments containing the specified text. ",
+    },
+    experimentStatusList: {
+      type: z.array(z.enum(["DRAFT", "EXECUTED", "COMPLETED", "ARCHIVED"])),
+      description:
+        "Parameter that accepts an array of experiment status values to filter experiments by their current status.",
+    },
   };
 
-  async execute({ pageNumber = 0, pageSize = 100 }: ExperimentListInput) {
+  async execute({
+    pageNumber = 0,
+    pageSize = 100,
+    searchKeyword = "",
+    experimentStatusList,
+  }: ExperimentListInput) {
     return await WebClient.get(
-      `/api/v2/experiments?environmentKey=production&pageNumber=${pageNumber}&pageSize=${pageSize}`
+      `/api/v2/experiments?pageNumber=${pageNumber}&pageSize=${pageSize}&searchKeyword=${searchKeyword}&experimentStatusList=${
+        experimentStatusList?.toString() ?? ""
+      }`
     );
   }
 }
---
+++ src/tools/RetentionSeriesTool.ts
@@ -0,0 +1,33 @@
+import { MCPTool } from "mcp-framework";
+import { z } from "zod";
+import WebClient from "../module/WebClient.js";
+
+interface RetentionSeriesInput {
+  unit: "DAY" | "WEEK" | "MONTH";
+  date?: string;
+}
+
+class RetentionSeriesTool extends MCPTool<RetentionSeriesInput> {
+  name = "retention-series";
+  description = "Fetches user retention series";
+
+  schema = {
+    unit: {
+      type: z.enum(["DAY", "WEEK", "MONTH"]),
+      description:
+        "Specifies the time unit for metric data. Data can be aggregated on a daily (DAY), weekly (WEEK), or monthly (MONTH) basis.",
+    },
+    date: {
+      type: z.string().optional(),
+      description: "The date in yyyy-mm-dd format to query to (inclusive)",
+    },
+  };
+
+  async execute({ unit = "DAY", date }: RetentionSeriesInput) {
+    return await WebClient.get(
+      `/api/v1/workspaces/auto-metrics/retention-series?unit=${unit}&date=${date}`
+    );
+  }
+}
+
+export default RetentionSeriesTool;
