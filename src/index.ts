1dac163dbf80936bbec646607dec98518e5a058f
Mobie Kim <q.kim@hackle.io>
2025-05-12 10:48:24 +0900

    Add remote config create, update tools

--- src/index.ts
+++ src/index.ts
@@ -365,5 +365,172 @@ server.tool(
   },
 );
 
+// Remote Config Creation Tool
+server.tool(
+  'remote-config-create',
+  'Creates an empty remote config. It is recommended to update an existing RC first if there is an associated RC with the one you want to create since the total number of RC is limited.',
+  {
+    body: z.object({
+      key: z.string().nonempty().describe("Remote config's name."),
+      description: z.string().optional(),
+      dataType: z.enum(['STRING', 'JSON', 'NUMBER', 'BOOLEAN']).describe("Type of Remote Config's value."),
+      userIdentifierCriteria: z
+        .string()
+        .optional()
+        .default('$deviceId')
+        .describe(
+          'User identifier criteria for targeting. You can use criteria provided by Hackle($deviceId, $userId) or your own criteria created at Hackle dashboard website.',
+        ),
+    }),
+  },
+  async ({ body }) => {
+    return {
+      content: [
+        {
+          type: 'text',
+          text: JSON.stringify(await WebClient.post(`/api/v1/remote-configs`, body)),
+        },
+      ],
+    };
+  },
+);
+
+// Remote Config Content Update Tool
+server.tool(
+  'remote-config-update',
+  "Updates remote config's content.",
+  {
+    remoteConfigId: z.number().positive().describe("Remote config's id."),
+    body: z.object({
+      dataType: z
+        .enum(['STRING', 'JSON', 'NUMBER', 'BOOLEAN'])
+        .describe(
+          "Type of Remote Config's value. You must provide remote config's value type to match with this field.",
+        ),
+      remoteConfigDefaultValue: z.union([z.string(), z.number(), z.boolean()]),
+      conditionalValues: z
+        .array(
+          z.object({
+            ruleName: z.string(),
+            remoteConfigValue: z.union([z.string(), z.number(), z.boolean()]),
+            target: z
+              .object({
+                conditions: z
+                  .array(
+                    z.object({
+                      key: z.object({
+                        type: z
+                          .enum(['HACKLE_PROPERTY', 'USER_PROPERTY', 'SEGMENT', 'AB_TEST', 'FEATURE_FLAG', 'COHORT'])
+                          .describe("Condition's type."),
+                        name: z
+                          .string()
+                          .describe(
+                            "Property's name if type is HACKLE_PROPERTY or USER_PROPERTY. Experiment key if type is AB_TEST. Feature flag key if type is FEATURE_FLAG. You can put any non-empty string if type is COHORT or SEGMENT.",
+                          ),
+                      }),
+                      match: z.object({
+                        operator: z.enum([
+                          'IS_ONE_OF',
+                          'IS_NOT_ONE_OF',
+                          'IS_STARTS_WITH',
+                          'IS_NOT_STARTS_WITH',
+                          'IS_ENDS_WITH',
+                          'IS_NOT_ENDS_WITH',
+                          'IS_CONTAINS',
+                          'IS_NOT_CONTAINS',
+                          'EQ',
+                          'NOT_EQ',
+                          'GT',
+                          'GTE',
+                          'LT',
+                          'LTE',
+                          'IS_TRUE',
+                          'IS_FALSE',
+                          'VERSION_EQ',
+                          'VERSION_NOT_EQ',
+                          'VERSION_GT',
+                          'VERSION_GTE',
+                          'VERSION_LT',
+                          'VERSION_LTE',
+                        ]),
+                        valueType: z.enum(['NUMBER', 'STRING', 'BOOLEAN', 'VERSION']),
+                        values: z
+                          .array(z.union([z.string(), z.number(), z.boolean()]))
+                          .describe(
+                            "Values of targeting condition's key. Followings are some special cases: The values will be treated as names if you are using SEGMENT. Only strings 'A' and 'B' are allowed if type is AB_TEST. Only boolean values are accepted if type is FEATURE_FLAG. You should put cohort's id if type is COHORT.",
+                          ),
+                      }),
+                    }),
+                  )
+                  .describe(
+                    "Users who The user he satisfies all conditions in this array will see this rule's remote config value.",
+                  ),
+              })
+              .describe('Targeting rule.'),
+          }),
+        )
+        .describe('The earlier a condition is placed in the array, the earlier it is applied.'),
+    }),
+  },
+  async ({ remoteConfigId, body }) => {
+    return {
+      content: [
+        {
+          type: 'text',
+          text: JSON.stringify(await WebClient.put(`/api/v1/remote-configs/${remoteConfigId}/parameters`, body)),
+        },
+      ],
+    };
+  },
+);
+
+// Remote Config User Identifier Criteria Update Tool
+server.tool(
+  'remote-config-update-user-identifier-criteria',
+  "Updates remote config's user identifier criteria. The change will be applied to both production and development environment.",
+  {
+    remoteConfigId: z.number().positive().describe("Remote config's id."),
+    body: z.object({
+      userIdentifierCriteria: z
+        .string()
+        .describe(
+          'User identifier criteria for targeting. You can use criteria provided by Hackle($deviceId, $userId) or your own criteria created at Hackle dashboard website.',
+        ),
+    }),
+  },
+  async ({ body, remoteConfigId }) => {
+    return {
+      content: [
+        {
+          type: 'text',
+          text: JSON.stringify(await WebClient.patch(`/api/v1/remote-configs/${remoteConfigId}/identifierType`, body)),
+        },
+      ],
+    };
+  },
+);
+
+// Remote Config Description Update Tool
+server.tool(
+  'remote-config-update-description',
+  "Updates remote config's description. The change will be applied to both production and development environment.",
+  {
+    remoteConfigId: z.number().positive().describe("Remote config's id."),
+    body: z.object({
+      description: z.string(),
+    }),
+  },
+  async ({ body, remoteConfigId }) => {
+    return {
+      content: [
+        {
+          type: 'text',
+          text: JSON.stringify(await WebClient.patch(`/api/v1/remote-configs/${remoteConfigId}/description`, body)),
+        },
+      ],
+    };
+  },
+);
+
 const transport = new StdioServerTransport();
 await server.connect(transport);
